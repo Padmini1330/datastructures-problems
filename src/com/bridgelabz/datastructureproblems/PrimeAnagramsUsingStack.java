157080198d4cd62dd804a78282cc31c047752b4f
padmini v <padmini.v@perfios.com>
2021-09-04 01:15:35 +0530

    ADDED: Program to print prime anagrams using stack

---
+++ src/com/bridgelabz/datastructureproblems/PrimeAnagramsUsingStack.java
@@ -0,0 +1,75 @@
+package com.bridgelabz.datastructureproblems;
+
+import java.util.Arrays;
+
+import com.bridgelabz.stackandqueueproblem.MyStack;
+import com.bridgelabz.stackandqueueproblem.Node;
+
+public class PrimeAnagramsUsingStack 
+{
+	private final static int LOWER_RANGE = 2;
+	private final static int UPPER_RANGE = 1000;
+
+	public static void main(String[] args) {
+		Integer[] prime = new Integer[200];
+		int arrayIndex = 0;
+		for (int i = LOWER_RANGE; i <= UPPER_RANGE; i++) {
+			if (isPrime(i)) {
+				prime[arrayIndex++] = i;
+			}
+		}
+		Node<Integer> firstNode, secondNode;
+		MyStack<Integer> myStack = new MyStack<Integer>();
+		for (int i = 0; i < arrayIndex - 2; i++) 
+		{
+			for (int j = i + 1; j < arrayIndex - 1; j++) 
+			{
+				if (isAnagram(prime[i] + "", prime[j] + "")) 
+				{
+					myStack.push(new Node<Integer>(prime[i]));
+					myStack.push(new Node<Integer>(prime[j]));
+
+				}
+			}
+		}
+		myStack.popAll();
+
+	}
+
+	
+	public static boolean isPrime(int number) 
+	{
+		int factors = 0;
+		for (int j = 2; j <= Math.sqrt(number); j++) 
+		{
+			if ((number % j) == 0) 
+			{
+				factors++;
+				break;
+			} 
+			else
+				continue;
+		}
+		if (factors == 0) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+	static boolean isAnagram(String number1, String number2) 
+	{
+
+		char[] num1 = number1.toString().toCharArray();
+		char[] num2 = number2.toString().toCharArray();
+		Arrays.sort(num1);
+		Arrays.sort(num2);
+		num1.toString();
+		num2.toString();
+		if ((new String(num1)).equals(new String(num2)))
+			return true;
+		else
+			return false;
+	}
+
+}
