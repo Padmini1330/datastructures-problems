e3a5111574129fc153ac3140ea8af182f99d34d0
padmini v <padmini.v@perfios.com>
2021-09-05 14:28:34 +0530

    REFACTORED: Added search feature and getIndex method

--- src/com/bridgelabz/datastructureproblems/HashMapInsertion.java
+++ src/com/bridgelabz/datastructureproblems/HashMapInsertion.java
@@ -1,34 +1,59 @@
 package com.bridgelabz.datastructureproblems;
 
-import com.bridgelabz.hashtableproblem.*;
+import java.util.Scanner;
+import java.util.SortedMap;
+
+import com.bridgelabz.linkedlistproblems.*;
 
 public class HashMapInsertion 
 {
 
 	public static void main(String[] args) 
 	{
-		String sentence= "To be or not to be";
-		
-		MyLinkedHashMap<String, Integer> linkedHashMap = new MyLinkedHashMap<String, Integer>();
-		String[] words = sentence.toLowerCase().split(" ");
-		
-		for(String word:words) 
+		Integer[] number= {2,5,12,1,14,50,21,30,2,5};
+		MySortedLinkedList<Integer>[] linkedList=new MySortedLinkedList[11];
+		MyNode<Integer> newNode;
+		for(int index=0;index<=10;index++) {
+			linkedList[index] = new MySortedLinkedList<Integer>();
+		}
+		for(Integer num:number)
 		{
-			Integer value = linkedHashMap.get(word);
-			if(value==null)
-			{
-				value=1;
-			}
-				
-			else 
-			{
-				value=value+1;
-			}
-				
-			linkedHashMap.add(word, value);	
-		}		
-		System.out.println(linkedHashMap);
+			newNode=new MyNode<Integer>(num);
+			int index=getIndex(num);
+			linkedList[index].append(newNode);
+		}
+		printNodes(linkedList);
+		
+		
+		System.out.println("Enter the number to search:");
+		Scanner scanner=new Scanner(System.in);
+		Integer key=scanner.nextInt();
+		int keyIndex=getIndex(key);
+		if(linkedList[keyIndex].find(key)!=null)
+		{
+			linkedList[keyIndex].delete(key);
+			printNodes(linkedList);
+		}
+		else
+		{
+			newNode=new MyNode<Integer>(key);
+			linkedList[keyIndex].append(newNode);
+			linkedList[keyIndex].printMyNodes();
+			printNodes(linkedList);
+		}
+		
 	}
+	public static int getIndex(int number) 
+	{
+		return number%11;
+	}
+	public static void printNodes(MySortedLinkedList<Integer>[] ob) {
+		for(int i=0;i<11;i++) {
+			System.out.println("   ");
+			System.out.print(i+"-->");
+			ob[i].printMyNodes();
+		}
+	}	
 
 	
 }
